d120e6f959f81db4a5a660e1986afbe2d080b6b9
blutungpu <blutungpu@gmail.com>
2022-07-07 12:18:02 +0800

    add

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from "redux";
+import accountPrefReducer from "./ducks/accountPref";
 import blogAuthReducer from "./ducks/blogAuth";
 
 const reducer = combineReducers({
-    auth: blogAuthReducer
-})
+  auth: blogAuthReducer,
+  accountPrefs: accountPrefReducer
+});
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 export default store;
--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
-import { commonStyles, lightStyles } from "../styles/commonStyles";
+import {
+  ActivityIndicator,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 import axios from "axios";
 import { API, API_WHOAMI } from "../constants/API";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
+import { lightModeAction, darkModeAction } from "../redux/ducks/accountPref";
+import {logOutAction} from "../redux/ducks/blogAuth";
 
 export default function AccountScreen({ navigation }) {
-  const token = useSelector((state) => state.auth.token);
   const [username, setUsername] = useState(null);
-  const styles = { ...commonStyles, ...lightStyles };
+  const token = useSelector((state) => state.auth.token);
+  const isDark = useSelector((state) => state.accountPrefs.isDark);
+  const profilePicture = useSelector(
+    (state) => state.accountPrefs.profilePicture
+  );
+  const styles = { ...commonStyles, ...(isDark ? darkStyles : lightStyles) };
+  const dispatch = useDispatch();
+
+  function switchMode() {
+    dispatch(isDark ? lightModeAction() : darkModeAction());
+  }
 
   async function getUsername() {
     console.log("---- Getting user name ----");
@@ -35,6 +53,7 @@ export default function AccountScreen({ navigation }) {
   }
 
   function signOut() {
+    dispatch(logOutAction());
     navigation.navigate("SignInSignUp");
   }
 
@@ -52,8 +71,29 @@ export default function AccountScreen({ navigation }) {
 
   return (
     <View style={[styles.container, { alignItems: "center" }]}>
-      <Text style={{ marginTop: 20 }}>Account Screen</Text>
-      <Text>{username}</Text>
+      <Text style={[styles.title, styles.text, { marginTop: 30 }]}>
+        Hello {username} !
+      </Text>
+      <Image source={{uri: profilePicture}}/>
+      <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+        <Text style={{ marginTop: 10, fontSize: 20, color: "#0000EE" }}>
+          No profile picture. Click to take one. {" "}
+        </Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: 20,
+        }}
+      >
+        <Text style={[styles.content, styles.text]}> Dark Mode? </Text>
+        <Switch value={isDark} onChange={switchMode} />
+      </View>
+      <TouchableOpacity style={[styles.button]} onPress={signOut}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
